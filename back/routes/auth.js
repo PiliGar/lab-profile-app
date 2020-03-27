507dfe1f133aa968b8a4beee81d3f6e5c3e6706d
Pilar <p.garciacampo@gmail.com>
2020-03-27 21:18:13 +0100

    Add routes

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -18,7 +18,16 @@ router.post("/signup", async (req, res, next) => {
 
   // Directly login user
   req.logIn(newUser, err => {
-    res.json(_.pick(req.user, ["username", "_id", "createdAt", "updatedAt"]));
+    res.json(
+      _.pick(req.user, [
+        "username",
+        "_id",
+        "campus",
+        "course",
+        "createdAt",
+        "updatedAt"
+      ])
+    );
   });
 });
 
@@ -26,7 +35,16 @@ router.post("/signup", async (req, res, next) => {
 router.post("/login", passport.authenticate("local"), (req, res) => {
   // Return the logged in user
   return res.json(
-    _.pick(req.user, ["username", "_id", "createdAt", "updatedAt"])
+    _.pick(
+      _.pick(req.user, [
+        "username",
+        "_id",
+        "campus",
+        "course",
+        "createdAt",
+        "updatedAt"
+      ])
+    )
   );
 });
 
@@ -42,9 +60,55 @@ router.get("/logout", (req, res, next) => {
   }
 });
 
+/* EDIT */
+router.post("/edit", async (req, res, next) => {
+  try {
+    const id = req.user._id;
+    const { username, campus, course } = req.body;
+    await Users.findByIdAndUpdate(id, {
+      username,
+      campus,
+      course
+    });
+    return res.json({ status: "User profile edited!" });
+  } catch (error) {
+    return res.status(401).json({ status: "Not Found" });
+  }
+});
+
+/* UPLOAD */
+router.post("/upload", async (req, res, next) => {
+  try {
+    const id = req.user._id;
+    const { image } = req.body;
+    await Users.findByIdAndUpdate(id, {
+      image
+    });
+    return res.json({ status: "User image edited!" });
+  } catch (error) {
+    return res.status(401).json({ status: "Not Found" });
+  }
+});
+
+/* EDIT */
+router.post("/edit", (req, res, next) => {
+  const { username, campus, course } = req.body;
+  res.json(`User updated ${username} ${campus} ${course}`);
+});
+
 // WHOAMI
 router.get("/whoami", (req, res, next) => {
-  if (req.user) return res.json(req.user);
+  if (req.user)
+    return res.json(
+      _.pick(req.user, [
+        "username",
+        "_id",
+        "campus",
+        "course",
+        "createdAt",
+        "updatedAt"
+      ])
+    );
   else return res.status(401).json({ status: "No user session present" });
 });
 
